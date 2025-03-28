c042b72a4e4ce3c66daff35608a7d6d315b7041e
APIVerve Team <hello@apiverve.com>
2025-03-28 18:02:26 -0500

    Bug Fixes

--- src/apiverve_useragentparser/apiClient.py
+++ src/apiverve_useragentparser/apiClient.py
@@ -10,10 +10,10 @@ class UseragentparserAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/useragentparser'
+        self.base_url = 'https://api.apiverve.com/v1/useragentparser'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
